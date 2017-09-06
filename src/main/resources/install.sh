79bf0853f9245e93055902ecf8d43fa666c3b8ce
puredanger <alex.miller@cognitect.com>
2017-09-06 11:30:39 -0500

    mkdir in install

--- src/main/resources/install.sh
+++ src/main/resources/install.sh
@@ -2,8 +2,15 @@
 
 prefix="$1"
 
-cp ./*.jar "$prefix/libexec"
+# default config file
 cp deps.edn "$prefix"
+
+# jar needed by scripts
+mkdir "$prefix/libexec"
+cp ./*.jar "$prefix/libexec"
+
+# scripts
 ruby -pi.bak -e "gsub(/PREFIX/, '$prefix')" clojure
+mkdir "$prefix/bin"
 cp clojure "$prefix/bin"
 cp clj "$prefix/bin"
