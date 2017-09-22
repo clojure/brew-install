980c18ab7ca4a8b44c0ded674114e51f0055bee4
puredanger <alex.miller@cognitect.com>
2017-09-22 14:31:34 -0500

    update tools.deps

--- script/package.sh
+++ script/package.sh
@@ -19,9 +19,10 @@ echo "Building scripts tar file"
 cp target/classes/clojure target
 cp target/classes/clj target
 cp target/classes/deps.edn target
+cp target/classes/example-deps.edn target
 cp target/classes/install.sh target
 chmod +x target/clojure target/clj target/install.sh
-tar -cvzf "target/clojure-scripts-${version}.tar.gz" -Ctarget "clojure-scripts-${version}.jar" clojure clj deps.edn install.sh
+tar -cvzf "target/clojure-scripts-${version}.tar.gz" -Ctarget "clojure-scripts-${version}.jar" clojure clj deps.edn example-deps.edn install.sh
 
 # Create formula file
 echo "Creating formula file"
