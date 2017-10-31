247a4b9ffb98be76d34bf3b79ac5360bb3ddf44a
puredanger <alex.miller@cognitect.com>
2017-10-31 13:15:55 -0500

    update stable sha

--- src/main/resources/clojure.rb
+++ src/main/resources/clojure.rb
@@ -1,8 +1,8 @@
 class Clojure < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
-  url "https://download.clojure.org/install/brew/clojure-scripts-1.8.0.179.tar.gz"
-  sha256 "38368bf8574ab7545ccfa7be7589a49007cc6865eae3f8d3a308298952ce649b"
+  url "https://download.clojure.org/install/brew/clojure-scripts-1.8.0.181.tar.gz"
+  sha256 "f6cb56d75b0668f52734386d92f3579e3e9bd5d0491df2bcd94b445e539d0f55"
 
   devel do
     url "https://download.clojure.org/install/brew/clojure-scripts-${project.version}.tar.gz"
