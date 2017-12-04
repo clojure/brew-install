9c7ddf868df7dca69e52a88fe50f436f54799a51
puredanger <alex.miller@cognitect.com>
2017-12-04 13:55:30 -0600

    1.9 stable

--- src/main/resources/clojure.rb
+++ src/main/resources/clojure.rb
@@ -1,14 +1,8 @@
 class Clojure < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
-  url "https://download.clojure.org/install/clojure-scripts-1.8.0.193.tar.gz"
-  sha256 "82e671b252362e1bdff2d5ffc8b5ef758df29f4b6503b41a79ee646c8c2651e1"
-
-  devel do
-    url "https://download.clojure.org/install/clojure-scripts-${project.version}.tar.gz"
-    sha256 "SHA"
-    version "${project.version}"
-  end
+  url "https://download.clojure.org/install/clojure-scripts-${project.version}.tar.gz"
+  sha256 "SHA"
 
   bottle :unneeded
 
