da1fc580754e70c78f1db765f0688f524a44946c
Andrea Richiardi <a.richiardi.work@gmail.com>
2018-02-23 12:24:08 -0600

    INST-9 Add --prefix command line option to linux-install.sh
    Signed-off-by: Alex Miller <alex.miller@cognitect.com>

--- src/main/resources/linux-install.sh
+++ src/main/resources/linux-install.sh
@@ -1,24 +1,58 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
+
+# Start
+do_usage() {
+  echo "Installs the Clojure command line tools."
+  echo -e
+  echo "Usage:"
+  echo "linux-install.sh [-p|--prefix <dir>]"
+  exit 1
+}
+
+default_prefix_dir="/usr/local"
+
+# use getopt if the number of params grows
+prefix_dir=$default_prefix_dir
+prefix_param=${1:-}
+prefix_value=${2:-}
+if [[ "$prefix_param" = "-p" || "$prefix_param" = "--prefix" ]]; then
+  if [[ -z "$prefix_value" ]]; then
+    do_usage
+  else
+    prefix_dir="$prefix_value"
+  fi
+fi
 
 echo "Downloading and expanding tar"
 curl -O https://download.clojure.org/install/clojure-tools-${project.version}.tar.gz
 tar xzf clojure-tools-${project.version}.tar.gz
 
-echo "Installing libs into /usr/local/lib/clojure"
-mkdir -p /usr/local/lib/clojure
-cp -f clojure-tools/deps.edn clojure-tools/example-deps.edn /usr/local/lib/clojure
-mkdir -p /usr/local/lib/clojure/libexec
-cp -f clojure-tools/clojure-tools-${project.version}.jar /usr/local/lib/clojure/libexec
+lib_dir="$prefix_dir/lib"
+bin_dir="$prefix_dir/bin"
+man_dir="$prefix_dir/share/man/man1"
+clojure_lib_dir="$lib_dir/clojure"
 
-echo "Installing clojure and clj into /usr/local/bin"
-sed -i -e 's@PREFIX@/usr/local/lib/clojure@g' clojure-tools/clojure
-cp -f clojure-tools/clojure clojure-tools/clj /usr/local/bin
+# we don't want to use -p because prefix dir should exist already, the prefix
+# folder should not be created but the necessary subfolders should.
 
-echo "Installing man pages into /usr/local/share/man/man1"
-cp -f clojure-tools/clojure.1 /usr/local/share/man/man1/
-cp -f clojure-tools/clj.1 /usr/local/share/man/man1/
+echo "Installing libs into $clojure_lib_dir"
+[[ ! -d "$lib_dir" ]] && mkdir "$lib_dir"
+[[ ! -d "$clojure_lib_dir" ]] && mkdir "$clojure_lib_dir"
+cp -f clojure-tools/deps.edn clojure-tools/example-deps.edn "$clojure_lib_dir"
+[[ ! -d "$clojure_lib_dir/libexec" ]] && mkdir "$clojure_lib_dir/libexec"
+cp -f clojure-tools/clojure-tools-${project.version}.jar "$clojure_lib_dir/libexec"
+
+echo "Installing clojure and clj into $bin_dir"
+[[ ! -d "$bin_dir" ]] && mkdir "$bin_dir"
+sed -i -e 's@PREFIX@'"$clojure_lib_dir"'@g' clojure-tools/clojure
+cp -f clojure-tools/clojure clojure-tools/clj "$bin_dir"
+
+echo "Installing man pages into $man_dir"
+[[ ! -d "$man_dir" ]] && mkdir "$man_dir"
+cp -f clojure-tools/clojure.1 "$man_dir"
+cp -f clojure-tools/clj.1 "$man_dir"
 
 echo "Removing download"
 rm -rf clojure-tools
