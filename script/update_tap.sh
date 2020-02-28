13bc60d34967bd5b113b5ec4a3f4ecd572d631c4
Alex Miller <alex.miller@cognitect.com>
2020-02-27 22:11:20 -0600

    add tap script

---
+++ script/update_tap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo "Building scripts version $version"
+
+# Grab version
+version=$(mvn -B help:evaluate -Dexpression=project.version 2>/dev/null| grep -v "^\[")
+
+# Checkout tap repo
+git clone git@github.com:clojure/homebrew-tools.git target/homebrew-tools
+
+# Copy in the new formulas
+cp target/clojure.rb target/homebrew-tools/Formula
+cp "target/clojure@$version.rb" target/homebrew-tools/Formula
+
+# Commit
+cd target/homebrew-tools
+git add clojure.rb "clojure@$version.rb"
+git commit -m 'Publish $version'
+git push
+cd ../..
