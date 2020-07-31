f3ebc4e6bf33ed6a4487e2f485b0881b32a36a47
Alex Miller <alex.miller@cognitect.com>
2020-07-31 14:05:37 -0500

    add error check for non-map data in -X :args

--- src/main/resources/clj_exec.clj
+++ src/main/resources/clj_exec.clj
@@ -81,7 +81,10 @@
   [alias overrides]
   (let [basis (read-basis)
         {f :fn, maybe-args :args} (get-in basis [:aliases alias])
-        args (if (keyword? maybe-args) (get-in basis [:aliases maybe-args]) maybe-args)]
+        args (cond
+               (keyword? maybe-args) (get-in basis [:aliases maybe-args])
+               (map? maybe-args) maybe-args
+               :else (throw (err (str "Invalid :args for exec, must be map or alias keyword: " (pr-str maybe-args)))))]
     (exec f (apply-overrides args overrides))))
 
 (defn- exec-fn
