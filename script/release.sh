7544cff58ec1e743569fba20cacf0f1a47e58f05
Alex Miller <alex.miller@cognitect.com>
2021-05-03 13:42:13 -0500

    tool support

--- script/release.sh
+++ script/release.sh
@@ -24,7 +24,7 @@ jar cf target/clojure-tools/exec.jar -C target/classes clojure/run/exec.clj
 # Make tar file of jar and script
 echo "Building scripts tar file"
 chmod +x target/classes/clojure/install/clojure target/classes/clojure/install/clj target/classes/clojure/install/install.sh target/classes/clojure/install/linux-install.sh
-cp target/classes/clojure/install/clojure target/classes/clojure/install/clj target/classes/clojure/install/deps.edn target/classes/clojure/install/example-deps.edn target/classes/clojure/install/install.sh "target/clojure-tools-$version.jar" doc/clojure.1 target/clojure-tools
+cp target/classes/clojure/install/clojure target/classes/clojure/install/clj target/classes/clojure/install/deps.edn target/classes/clojure/install/example-deps.edn target/classes/clojure/install/tools.edn target/classes/clojure/install/install.sh "target/clojure-tools-$version.jar" doc/clojure.1 target/clojure-tools
 cp "target/clojure-tools-$version.jar" "target/clojure-tools/clojure-tools-$version.jar"
 cp doc/clojure.1 target/clojure-tools/clj.1
 tar -cvzf "target/clojure-tools-$version.tar.gz" -Ctarget clojure-tools
@@ -32,7 +32,7 @@ tar -cvzf "target/clojure-tools-$version.tar.gz" -Ctarget clojure-tools
 # Make zip file of jar and windows scripts
 echo "Building scripts zip file"
 mkdir -p target/win/ClojureTools
-cp target/classes/clojure/install/ClojureTools.psd1 target/classes/clojure/install/ClojureTools.psm1 target/classes/clojure/install/deps.edn target/classes/clojure/install/example-deps.edn "target/clojure-tools-$version.jar" target/win/ClojureTools
+cp target/classes/clojure/install/ClojureTools.psd1 target/classes/clojure/install/ClojureTools.psm1 target/classes/clojure/install/deps.edn target/classes/clojure/install/example-deps.edn target/classes/clojure/install/tools.edn "target/clojure-tools-$version.jar" target/win/ClojureTools
 cp target/clojure-tools/exec.jar target/win/ClojureTools
 cd target/win
 zip -r "../clojure-tools.zip" ClojureTools
