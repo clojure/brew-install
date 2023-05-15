8b59752f7e989ce978a1f23a6310f37438cf7373
Alex Miller <alex.miller@cognitect.com>
2023-05-15 18:26:58 -0500

    update script and action for release to tap

--- script/update_tap.sh
+++ script/update_tap.sh
@@ -2,21 +2,18 @@
 
 set -e
 
-echo "Building scripts version $version"
+# Checkout tap repo - done by action
+# git clone git@github.com:clojure/homebrew-tools.git target/homebrew-tools
 
-# Grab version
-version=$(cat VERSION)
-
-# Checkout tap repo
-git clone git@github.com:clojure/homebrew-tools.git target/homebrew-tools
+echo "Update repo tap to add version $VERSION"
 
 # Copy in the new formulas
 cp target/clojure.rb target/homebrew-tools/Formula
-cp "target/clojure@$version.rb" target/homebrew-tools/Formula
+cp "target/clojure@$VERSION.rb" target/homebrew-tools/Formula
 
 # Commit
 cd target/homebrew-tools
-git add Formula/clojure.rb "Formula/clojure@$version.rb"
-git commit -m "Publish $version"
+git add Formula/clojure.rb "Formula/clojure@$VERSION.rb"
+git commit -m "Publish $VERSION"
 git push
 cd ../..
